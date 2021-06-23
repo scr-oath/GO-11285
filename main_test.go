c7a8c696e4807a445fe9d7fada5ecffb2ed72475
Sheridan C Rawlins <scr@verizonmedia.com>
2021-06-23 11:07:48 -0700

    Show an example test that hangs when run from GoLand launched from Toolbox.

---
+++ main_test.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestPassFail(t *testing.T) {
+	tests := []struct {
+		name string
+		pass bool
+	}{
+		{
+			name: "given pass=true, " +
+				"when assert.True is invoked, " +
+				"it always passes",
+			pass: true,
+		},
+		{
+			name: "given pass=false, " +
+				"when assert.True is invoked, " +
+				"it always fails",
+			pass: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Truef(t, tt.pass, "test doesn't pass")
+		})
+	}
+}
